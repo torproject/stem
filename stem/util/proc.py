df2084b0e746e6c5036528b01884a168b3b513ce
Damian Johnson <atagar@torproject.org>
2012-03-28 20:09:08 -0700

    Minor corrections for os.sysconf check
    The patch from beckbaibai fixed his issue but left CLOCK_TICKS undefined on
    Windows which would result in a NameError when using proc.get_stats (the
    function *should* cause an error, but not that one).

--- stem/util/proc.py
+++ stem/util/proc.py
@@ -33,10 +33,12 @@ import stem.util.log as log
 
 # cached system values
 IS_PROC_AVAILABLE, SYS_START_TIME, SYS_PHYSICAL_MEMORY = None, None, None
-try:
-  CLOCK_TICKS = os.sysconf(os.sysconf_names["SC_CLK_TCK"])
-except AttributeError:
-  pass
+CLOCK_TICKS = None
+
+# os.sysconf is only defined on unix
+try: CLOCK_TICKS = os.sysconf(os.sysconf_names["SC_CLK_TCK"])
+except AttributeError: pass
+
 Stat = stem.util.enum.Enum(("COMMAND", "command"), ("CPU_UTIME", "utime"),
                  ("CPU_STIME", "stime"), ("START_TIME", "start time"))
 
@@ -225,6 +227,9 @@ def get_stats(pid, *stat_types):
   Raises:
     IOError if it can't be determined
   """
+  
+  if CLOCK_TICKS == None:
+    raise IOError("Unable to look up SC_CLK_TCK")
   
   start_time, parameter = time.time(), "process %s" % ", ".join(stat_types)
   
