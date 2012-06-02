5c9579626a131e8bd17c283e0904525e30fe4488
Damian Johnson <atagar@torproject.org>
2012-06-02 11:06:42 -0700

    Adding module metadata (author, version, etc)

--- stem/__init__.py
+++ stem/__init__.py
@@ -2,5 +2,13 @@
 Library for working with the tor process.
 """
 
+__version__ = '0.0.1'
+__author__ = 'Damian Johnson'
+__contact__ = 'atagar@torproject.org'
+__url__ = 'http://www.atagar.com/stem/'
+__license__ = 'LGPLv3'
+
 __all__ = ["descriptor", "response", "util", "connection", "control", "process", "socket", "version"]
+
+
 
