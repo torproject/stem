54a013552266540d5029af2ff08db2fed1c1dc92
Damian Johnson <atagar@torproject.org>
2012-06-24 14:14:49 -0700

    Stopping tor initialization timeout when done
    Ok, I'm kinda surprised that we haven't tripped over this before. We never
    terminate the tor initialization timeout on either or success or failure,
    causing us to arbitrarily kill our process after 90 seconds.
    
    This bug probably survived so long becuase our integ tests take less time than
    that to complete, and then spawns a new tor instance resetting the timer again.

--- stem/process.py
+++ stem/process.py
@@ -104,6 +104,8 @@ def launch_tor(tor_cmd = "tor", args = None, torrc_path = None, completion_perce
     
     # this will provide empty results if the process is terminated
     if not init_line:
+      if timeout: signal.alarm(0) # stop alarm
+      
       # ... but best make sure
       if stem.prereq.is_python_26():
         tor_process.kill()
@@ -120,6 +122,8 @@ def launch_tor(tor_cmd = "tor", args = None, torrc_path = None, completion_perce
     problem_match = problem_line.search(init_line)
     
     if bootstrap_match and int(bootstrap_match.groups()[0]) >= completion_percent:
+      if timeout: signal.alarm(0) # stop alarm
+      
       if temp_file:
         try: os.remove(temp_file)
         except: pass
