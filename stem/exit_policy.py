384e78df0594c7d60af4b4a0096b12d4419db7f8
Damian Johnson <atagar@torproject.org>
2012-07-17 07:58:14 -0700

    Correcting and expanding summary() example
    The ExitPolicy class no longer has a from_str() function. Also adding an
    example that better exemplifies how ranges work.

--- stem/exit_policy.py
+++ stem/exit_policy.py
@@ -148,9 +148,13 @@ class ExitPolicy(object):
     
     ::
     
-      >>> policy = ExitPolicy.from_str("accept *:80, accept *:443, reject *:*")
+      >>> policy = ExitPolicy('accept *:80', 'accept *:443', 'reject *:*')
       >>> policy.summary()
       "accept 80, 443"
+      
+      >>> policy = ExitPolicy('accept *:443', 'reject *:1-1024', 'accept *:*')
+      >>> policy.summary()
+      "reject 1-442, 444-1024"
     
     :returns: str with a concise summary for our policy
     """
