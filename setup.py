4c65738d100ab0802a75cc4b9d64065c8a8ef881
Damian Johnson <atagar@torproject.org>
2012-11-25 13:03:46 -0800

    Borrowing setup.py corrections from robinson
    Borrowing a couple things from robinson's patch on...
    https://trac.torproject.org/7565
    
    ... including the description, updated stem url, and provides/keywords args.
    Tested by simply running 'python setup.py build'.

--- setup.py
+++ setup.py
@@ -8,6 +8,11 @@ from stem import __version__, \
 
 from distutils.core import setup
 
+DESCRIPTION = """\
+Stem is a python controller library for Tor <https://www.torproject.org/>.
+Like its predecessor, TorCtl, it uses Tor's control protocol to help
+developers program against the Tor process."""
+
 try:
   from distutils.command.build_py import build_py_2to3 as build_py
 except ImportError:
@@ -15,12 +20,14 @@ except ImportError:
 
 setup(name = 'stem',
       version = __version__,
-      description = 'Controller library for tor',
+      description = DESCRIPTION,
       license = __license__,
       author = __author__,
       author_email = __contact__,
       url = __url__,
       packages = ['stem', 'stem.descriptor', 'stem.response', 'stem.util'],
+      provides = ['stem'],
       cmdclass = {'build_py': build_py},
+      keywords = "tor onion controller",
 )
 
