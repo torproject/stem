84664f1d6f388c93ff686dd049dfe48804ead41f
Damian Johnson <atagar@torproject.org>
2013-01-12 21:07:47 -0800

    Missing files for prior commit
    Oops. Did a 'git commit -a' while forgetting that we were adding a couple new
    files.

---
+++ test/unit/descriptor/networkstatus/bridge_document.py
@@ -0,0 +1,45 @@
+"""
+Unit tests for the BridgeNetworkStatusDocument of stem.descriptor.networkstatus.
+"""
+
+import datetime
+import unittest
+
+from stem.descriptor.networkstatus import BridgeNetworkStatusDocument
+
+DOCUMENT = """\
+published 2012-06-01 04:07:04
+r Unnamed ABSiBVJ42z6w5Z6nAXQUFq8YVVg FI74aFuNJZZQrgln0f+OaocMd0M 2012-05-31 15:57:00 10.97.236.247 443 0
+s Valid
+w Bandwidth=55
+p reject 1-65535
+r TolFuin AFn9TveYjdtZEsgh7QsWp3qC5kU 1Sw8RPx2Tq/w+VHL+pZipiJUG5k 2012-05-31 18:12:39 10.99.47.37 80 0
+s Fast Guard Running Stable Valid
+w Bandwidth=32
+p reject 1-65535
+"""
+
+
+class TestBridgeNetworkStatusDocument(unittest.TestCase):
+  def test_empty_document(self):
+    """
+    Parse a document without any router status entries.
+    """
+
+    document = BridgeNetworkStatusDocument("published 2012-06-01 04:07:04")
+    self.assertEqual(datetime.datetime(2012, 6, 1, 4, 7, 4), document.published)
+    self.assertEqual((), document.routers)
+    self.assertEqual([], document.get_unrecognized_lines())
+
+  def test_document(self):
+    """
+    Parse a document with router status entries.
+    """
+
+    document = BridgeNetworkStatusDocument(DOCUMENT)
+    self.assertEqual(datetime.datetime(2012, 6, 1, 4, 7, 4), document.published)
+
+    self.assertEqual(2, len(document.routers))
+    self.assertEqual("Unnamed", document.routers[0].nickname)
+    self.assertEqual("TolFuin", document.routers[1].nickname)
+    self.assertEqual([], document.get_unrecognized_lines())
