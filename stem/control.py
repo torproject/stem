torproject/stem

commit b12e9c01c318863f4e4cddacf341abfd197ff412

Storing raw descriptor content as bytes
Tor descriptors are byte strings. While most content is ASCII (and hence maps
to UTF8) there are a couple exceptions: the server descriptor's contact and
platform lines which are arbitrary byte content. Usually this is unicode but on
occasion it isn't (14 of 3044 in my old consensus cache).

In those cases we converted the content to unicode and replaced the
un-decodeable characters. This in turn caused our _verify_digest() check to
report the descriptor content as being malformed if the user had pycrypto
installed.

Changing the raw contents the Descriptor class tracks to be bytes instead of
unicode. This actually leads to far more elegant code, letting us drop the
UnicodeReader which was always a pretty gross hack.

This breaks python 2.5 compatability by using the io.BytesIO class. Presently
I'm checking with tor-dev@ to see if anyone objects with dropping python 2.5
support entirely...

https://lists.torproject.org/pipermail/tor-dev/2013-March/004551.html

If the answer is...

* yes, drop support: we'll replace test.mocking.BytesBuffer with io.BytesIO
  (they both do the exact same thing)

* no, keep support: we'll need to move test.mocking.BytesBuffer to a util and
  use it instead of io.BytesIO

stem/control.py
@@ -136,6 +136,7 @@ providing its own for interacting at a higher level.
 
 from __future__ import with_statement
 
+import io
 import os
 import Queue
 import StringIO
@@ -156,7 +157,7 @@ import stem.util.tor_tools
 import stem.version
 
 from stem import UNDEFINED, CircStatus, Signal
-from stem.util import log
+from stem.util import log, str_tools
 
 # state changes a control socket can have
 
@@ -1120,7 +1121,7 @@ class Controller(BaseController):
 
       desc_content = self.get_info("desc/all-recent")
 
-      for desc in stem.descriptor.server_descriptor._parse_file(StringIO.StringIO(desc_content)):
+      for desc in stem.descriptor.server_descriptor._parse_file(io.BytesIO(str_tools._to_bytes(desc_content))):
         yield desc
     except Exception, exc:
       if default == UNDEFINED:
@@ -1190,7 +1191,7 @@ class Controller(BaseController):
       desc_content = self.get_info("ns/all")
 
       desc_iterator = stem.descriptor.router_status_entry._parse_file(
-        StringIO.StringIO(desc_content),
+        io.BytesIO(str_tools._to_bytes(desc_content)),
         True,
         entry_class = stem.descriptor.router_status_entry.RouterStatusEntryV2,
       )

stem/descriptor/__init__.py
@@ -143,10 +143,6 @@ def parse_file(descriptor_file, descriptor_type = None, validate = True, documen
   import stem.descriptor.extrainfo_descriptor
   import stem.descriptor.networkstatus
 
-  # attempt to read content as unicode
-
-  descriptor_file = _UnicodeReader(descriptor_file)
-
   # The tor descriptor specifications do not provide a reliable method for
   # identifying a descriptor file's type and version so we need to guess
   # based on its filename. Metrics descriptors, however, can be identified
@@ -154,13 +150,14 @@ def parse_file(descriptor_file, descriptor_type = None, validate = True, documen
   # https://trac.torproject.org/5651
 
   initial_position = descriptor_file.tell()
-  first_line = descriptor_file.readline().strip()
+  first_line = stem.util.str_tools._to_unicode(descriptor_file.readline().strip())
   metrics_header_match = re.match("^@type (\S+) (\d+).(\d+)$", first_line)
 
   if not metrics_header_match:
     descriptor_file.seek(initial_position)
 
-  filename = '<undefined>' if descriptor_file.name is None else os.path.basename(descriptor_file.name)
+  descriptor_path = getattr(descriptor_file, 'name', None)
+  filename = '<undefined>' if descriptor_path is None else os.path.basename(descriptor_file.name)
   file_parser = None
 
   if descriptor_type is not None:
@@ -192,8 +189,8 @@ def parse_file(descriptor_file, descriptor_type = None, validate = True, documen
 
   if file_parser:
     for desc in file_parser(descriptor_file):
-      if descriptor_file.name is not None:
-        desc._set_path(os.path.abspath(descriptor_file.name))
+      if descriptor_path is not None:
+        desc._set_path(os.path.abspath(descriptor_path))
 
       yield desc
 
@@ -286,6 +283,17 @@ class Descriptor(object):
 
     return self._archive_path
 
+  def get_bytes(self):
+    """
+    Provides the ASCII **bytes** of the descriptor. This only differs from
+    **str()** if you're running python 3.x, in which case **str()** provides a
+    **unicode** string.
+
+    :returns: **bytes** for the descriptor's contents
+    """
+
+    return self._raw_contents
+
   def get_unrecognized_lines(self):
     """
     Provides a list of lines that were either ignored or had data that we did
@@ -305,61 +313,9 @@ class Descriptor(object):
 
   def __str__(self):
     if stem.prereq.is_python_3():
+      return stem.util.str_tools._to_unicode(self._raw_contents)
+    else:
       return self._raw_contents
-    else:
-      return str(stem.util.str_tools._to_bytes(self._raw_contents))
-
-
-class _UnicodeReader(object):
-  """
-  File-like object that wraps another file. This replaces read ASCII bytes with
-  unicode content. This only supports read operations.
-  """
-
-  def __init__(self, wrapped_file):
-    self.wrapped_file = wrapped_file
-    self.name = getattr(wrapped_file, 'name', None)
-
-  def close(self):
-    return self.wrapped_file.close()
-
-  def getvalue(self):
-    return self.wrapped_file.getvalue()
-
-  def isatty(self):
-    return self.wrapped_file.isatty()
-
-  def next(self):
-    return self.wrapped_file.next()
-
-  def read(self, n = -1):
-    return stem.util.str_tools._to_unicode(self.wrapped_file.read(n))
-
-  def readline(self):
-    return stem.util.str_tools._to_unicode(self.wrapped_file.readline())
-
-  def readlines(self, sizehint = None):
-    # being careful to do in-place conversion so we don't accidently double our
-    # memory usage
-
-    if sizehint is not None:
-      results = self.wrapped_file.readlines(sizehint)
-    else:
-      results = self.wrapped_file.readlines()
-
-    for i in xrange(len(results)):
-      results[i] = stem.util.str_tools._to_unicode(results[i])
-
-    return results
-
-  def seek(self, pos, mode = None):
-    if mode is not None:
-      return self.wrapped_file.seek(pos, mode)
-    else:
-      return self.wrapped_file.seek(pos)
-
-  def tell(self):
-    return self.wrapped_file.tell()
 
 
 def _read_until_keywords(keywords, descriptor_file, inclusive = False, ignore_first = False, skip = False, end_position = None, include_ending_keyword = False):
@@ -402,11 +358,11 @@ def _read_until_keywords(keywords, descriptor_file, inclusive = False, ignore_fi
     if not line:
       break  # EOF
 
-    line_match = KEYWORD_LINE.match(line)
+    line_match = KEYWORD_LINE.match(stem.util.str_tools._to_unicode(line))
 
     if not line_match:
       # no spaces or tabs in the line
-      line_keyword = line.strip()
+      line_keyword = stem.util.str_tools._to_unicode(line.strip())
     else:
       line_keyword = line_match.groups()[0]
 

stem/response/events.py
@@ -3,8 +3,8 @@
 # See LICENSE for licensing information
 
 import datetime
+import io
 import re
-import StringIO
 import time
 
 import stem
@@ -597,7 +597,7 @@ class NetworkStatusEvent(Event):
     content = str(self).lstrip("NS\n").rstrip("\nOK")
 
     self.desc = list(stem.descriptor.router_status_entry._parse_file(
-      StringIO.StringIO(content),
+      io.BytesIO(str_tools._to_bytes(content)),
       True,
       entry_class = stem.descriptor.router_status_entry.RouterStatusEntryV3,
     ))
@@ -622,7 +622,7 @@ class NewConsensusEvent(Event):
     content = str(self).lstrip("NEWCONSENSUS\n").rstrip("\nOK")
 
     self.desc = list(stem.descriptor.router_status_entry._parse_file(
-      StringIO.StringIO(content),
+      io.BytesIO(str_tools._to_bytes(content)),
       True,
       entry_class = stem.descriptor.router_status_entry.RouterStatusEntryV3,
     ))

stem/descriptor/extrainfo_descriptor.py
@@ -164,9 +164,9 @@ def _parse_file(descriptor_file, is_bridge = False, validate = True):
 
     if extrainfo_content:
       if is_bridge:
-        yield BridgeExtraInfoDescriptor("".join(extrainfo_content), validate)
+        yield BridgeExtraInfoDescriptor(bytes.join(b"", extrainfo_content), validate)
       else:
-        yield RelayExtraInfoDescriptor("".join(extrainfo_content), validate)
+        yield RelayExtraInfoDescriptor(bytes.join(b"", extrainfo_content), validate)
     else:
       break  # done parsing file
 
@@ -322,6 +322,7 @@ class ExtraInfoDescriptor(stem.descriptor.Descriptor):
     """
 
     super(ExtraInfoDescriptor, self).__init__(raw_contents)
+    raw_contents = stem.util.str_tools._to_unicode(raw_contents)
 
     self.nickname = None
     self.fingerprint = None

stem/descriptor/microdescriptor.py
@@ -69,7 +69,7 @@ def _parse_file(descriptor_file, validate = True):
 
       if not line:
         break  # EOF
-      elif line.startswith("@") or line.startswith("onion-key"):
+      elif line.startswith(b"@") or line.startswith(b"onion-key"):
         descriptor_file.seek(last_position)
         break
       else:
@@ -77,9 +77,9 @@ def _parse_file(descriptor_file, validate = True):
 
     if descriptor_lines:
       # strip newlines from annotations
-      annotations = map(unicode.strip, annotations)
+      annotations = map(bytes.strip, annotations)
 
-      descriptor_text = "".join(descriptor_lines)
+      descriptor_text = bytes.join(b"", descriptor_lines)
 
       yield Microdescriptor(descriptor_text, validate, annotations)
     else:
@@ -105,6 +105,7 @@ class Microdescriptor(stem.descriptor.Descriptor):
 
   def __init__(self, raw_contents, validate = True, annotations = None):
     super(Microdescriptor, self).__init__(raw_contents)
+    raw_contents = stem.util.str_tools._to_unicode(raw_contents)
 
     self.onion_key = None
     self.ntor_onion_key = None
@@ -143,8 +144,8 @@ class Microdescriptor(stem.descriptor.Descriptor):
       annotation_dict = {}
 
       for line in self._annotation_lines:
-        if " " in line:
-          key, value = line.split(" ", 1)
+        if b" " in line:
+          key, value = line.split(b" ", 1)
           annotation_dict[key] = value
         else:
           annotation_dict[line] = None

stem/descriptor/networkstatus.py
@@ -47,7 +47,7 @@ For more information see :func:`~stem.descriptor.__init__.DocumentHandler`...
 """
 
 import datetime
-import StringIO
+import io
 
 import stem.descriptor
 import stem.descriptor.router_status_entry
@@ -201,7 +201,7 @@ def _parse_file(document_file, document_type = None, validate = True, is_microde
   routers_end = document_file.tell()
 
   footer = document_file.readlines()
-  document_content = "".join(header + footer)
+  document_content = bytes.join(b"", header + footer)
 
   if document_handler == stem.descriptor.DocumentHandler.BARE_DOCUMENT:
     yield document_type(document_content, validate)
@@ -265,7 +265,6 @@ class NetworkStatusDocumentV2(NetworkStatusDocument):
   """
 
   def __init__(self, raw_content, validate = True):
-    raw_content = stem.util.str_tools._to_unicode(raw_content)
     super(NetworkStatusDocumentV2, self).__init__(raw_content)
 
     self.version = None
@@ -289,8 +288,8 @@ class NetworkStatusDocumentV2(NetworkStatusDocument):
     # that header/footer attributes aren't in the wrong section. This is a
     # deprecated descriptor type - patches welcome if you want those checks.
 
-    document_file = StringIO.StringIO(raw_content)
-    document_content = "".join(stem.descriptor._read_until_keywords((ROUTERS_START, V2_FOOTER_START), document_file))
+    document_file = io.BytesIO(raw_content)
+    document_content = bytes.join(b"", stem.descriptor._read_until_keywords((ROUTERS_START, V2_FOOTER_START), document_file))
 
     self.routers = tuple(stem.descriptor.router_status_entry._parse_file(
       document_file,
@@ -301,7 +300,8 @@ class NetworkStatusDocumentV2(NetworkStatusDocument):
       extra_args = (self,),
     ))
 
-    document_content += "\n" + document_file.read()
+    document_content += b"\n" + document_file.read()
+    document_content = stem.util.str_tools._to_unicode(document_content)
 
     entries = stem.descriptor._get_descriptor_components(document_content, validate)
 
@@ -464,9 +464,8 @@ class NetworkStatusDocumentV3(NetworkStatusDocument):
     :raises: **ValueError** if the document is invalid
     """
 
-    raw_content = stem.util.str_tools._to_unicode(raw_content)
     super(NetworkStatusDocumentV3, self).__init__(raw_content)
-    document_file = StringIO.StringIO(raw_content)
+    document_file = io.BytesIO(raw_content)
 
     self._header = _DocumentHeader(document_file, validate, default_params)
 
@@ -561,7 +560,8 @@ class _DocumentHeader(object):
 
     self._unrecognized_lines = []
 
-    content = "".join(stem.descriptor._read_until_keywords((AUTH_START, ROUTERS_START, FOOTER_START), document_file))
+    content = bytes.join(b"", stem.descriptor._read_until_keywords((AUTH_START, ROUTERS_START, FOOTER_START), document_file))
+    content = stem.util.str_tools._to_unicode(content)
     entries = stem.descriptor._get_descriptor_components(content, validate)
     self._parse(entries, validate)
 
@@ -791,7 +791,7 @@ class _DocumentFooter(object):
     self.bandwidth_weights = {}
     self._unrecognized_lines = []
 
-    content = document_file.read()
+    content = stem.util.str_tools._to_unicode(document_file.read())
 
     if not content:
       return  # footer is optional and there's nothing to parse
@@ -1006,6 +1006,7 @@ class DirectoryAuthority(stem.descriptor.Descriptor):
     """
 
     super(DirectoryAuthority, self).__init__(raw_content)
+    raw_content = stem.util.str_tools._to_unicode(raw_content)
 
     self.nickname = None
     self.fingerprint = None
@@ -1201,6 +1202,7 @@ class KeyCertificate(stem.descriptor.Descriptor):
 
   def __init__(self, raw_content, validate = True):
     super(KeyCertificate, self).__init__(raw_content)
+    raw_content = stem.util.str_tools._to_unicode(raw_content)
 
     self.version = None
     self.address = None
@@ -1415,9 +1417,8 @@ class BridgeNetworkStatusDocument(NetworkStatusDocument):
     self.routers = None
     self.published = None
 
-    document_file = StringIO.StringIO(raw_content)
-
-    published_line = document_file.readline()
+    document_file = io.BytesIO(raw_content)
+    published_line = stem.util.str_tools._to_unicode(document_file.readline())
 
     if published_line.startswith("published "):
       published_line = published_line.split(" ", 1)[1].strip()
@@ -1428,7 +1429,7 @@ class BridgeNetworkStatusDocument(NetworkStatusDocument):
         if validate:
           raise ValueError("Bridge network status document's 'published' time wasn't parsable: %s" % published_line)
     elif validate:
-      raise ValueError("Bridge network status documents must start with a 'published' line:\n%s" % raw_content)
+      raise ValueError("Bridge network status documents must start with a 'published' line:\n%s" % stem.util.str_tools._to_unicode(raw_content))
 
     self.routers = tuple(stem.descriptor.router_status_entry._parse_file(
       document_file,

stem/descriptor/router_status_entry.py
@@ -64,7 +64,7 @@ def _parse_file(document_file, validate, entry_class, entry_keyword = "r", start
   # check if we're starting at the end of the section (ie, there's no entries to read)
   if section_end_keywords:
     first_keyword = None
-    line_match = stem.descriptor.KEYWORD_LINE.match(document_file.readline())
+    line_match = stem.descriptor.KEYWORD_LINE.match(stem.util.str_tools._to_unicode(document_file.readline()))
 
     if line_match:
       first_keyword = line_match.groups()[0]
@@ -83,7 +83,7 @@ def _parse_file(document_file, validate, entry_class, entry_keyword = "r", start
       include_ending_keyword = True
     )
 
-    desc_content = "".join(desc_lines)
+    desc_content = bytes.join(b"", desc_lines)
 
     if desc_content:
       yield entry_class(desc_content, validate, *extra_args)
@@ -129,6 +129,7 @@ class RouterStatusEntry(stem.descriptor.Descriptor):
     """
 
     super(RouterStatusEntry, self).__init__(content)
+    content = stem.util.str_tools._to_unicode(content)
 
     self.document = document
 

stem/descriptor/server_descriptor.py
@@ -127,9 +127,9 @@ def _parse_file(descriptor_file, is_bridge = False, validate = True):
 
     if descriptor_content:
       # strip newlines from annotations
-      annotations = map(unicode.strip, annotations)
+      annotations = map(bytes.strip, annotations)
 
-      descriptor_text = "".join(descriptor_content)
+      descriptor_text = bytes.join(b"", descriptor_content)
 
       if is_bridge:
         yield BridgeDescriptor(descriptor_text, validate, annotations)
@@ -209,6 +209,7 @@ class ServerDescriptor(stem.descriptor.Descriptor):
     """
 
     super(ServerDescriptor, self).__init__(raw_contents)
+    raw_contents = stem.util.str_tools._to_unicode(raw_contents)
 
     self.nickname = None
     self.fingerprint = None
@@ -302,8 +303,8 @@ class ServerDescriptor(stem.descriptor.Descriptor):
       annotation_dict = {}
 
       for line in self._annotation_lines:
-        if " " in line:
-          key, value = line.split(" ", 1)
+        if b" " in line:
+          key, value = line.split(b" ", 1)
           annotation_dict[key] = value
         else:
           annotation_dict[line] = None
@@ -652,9 +653,9 @@ class RelayDescriptor(ServerDescriptor):
       # Digest is calculated from everything in the
       # descriptor except the router-signature.
 
-      raw_descriptor = str(self)
-      start_token = "router "
-      sig_token = "\nrouter-signature\n"
+      raw_descriptor = self.get_bytes()
+      start_token = b"router "
+      sig_token = b"\nrouter-signature\n"
       start = raw_descriptor.find(start_token)
       sig_start = raw_descriptor.find(sig_token)
       end = sig_start + len(sig_token)

commit 88c3b1a6ae4152f1724fcc7267b7a5b69aae5a74

Changing isinstance() checks to look for both bytes and unicode
Commit b12e9c0 broke a couple of our integration tests because it caused us to
call controller methods with unicode rather than str arguments...

https://trac.torproject.org/8576

This should be fine - python 2.x can usually use bytes and unicode inputs
interchangeably. The problem was that we routinely use isinstance() checks to
see if an input is a string and, if it isn't, assume it's a collection. As a
result we were calling...

GETINFO m d / n a m e / f o o

... rather than...

GETINFO md/name/foo

Tor in turn errored saying that those single character getinfo options didn't
exist. To fix this I'm changing most of our isinstance() checks to look for
both bytes and unicode.

stem/control.py
@@ -737,7 +737,7 @@ class Controller(BaseController):
     start_time = time.time()
     reply = {}
 
-    if isinstance(params, str):
+    if isinstance(params, (bytes, unicode)):
       is_multiple = False
       params = set([params])
     else:
@@ -1299,7 +1299,7 @@ class Controller(BaseController):
     start_time = time.time()
     reply = {}
 
-    if isinstance(params, str):
+    if isinstance(params, (bytes, unicode)):
       params = [params]
 
     # remove strings which contain only whitespace
@@ -1483,7 +1483,7 @@ class Controller(BaseController):
           if value is None:
             if cache_key in self._request_cache:
               del self._request_cache[cache_key]
-          elif isinstance(value, str):
+          elif isinstance(value, (bytes, unicode)):
             self._request_cache[cache_key] = [value]
           else:
             self._request_cache[cache_key] = value
@@ -1697,7 +1697,7 @@ class Controller(BaseController):
       * :class:`stem.InvalidArguments` if features passed were invalid
     """
 
-    if isinstance(features, str):
+    if isinstance(features, (bytes, unicode)):
       features = [features]
 
     response = self.msg("USEFEATURE %s" % " ".join(features))
@@ -1848,7 +1848,7 @@ class Controller(BaseController):
 
       args = [circuit_id]
 
-      if isinstance(path, str):
+      if isinstance(path, (bytes, unicode)):
         path = [path]
 
       if path:

stem/descriptor/__init__.py
@@ -365,7 +365,7 @@ def _read_until_keywords(keywords, descriptor_file, inclusive = False, ignore_fi
   content = None if skip else []
   ending_keyword = None
 
-  if isinstance(keywords, str):
+  if isinstance(keywords, (bytes, unicode)):
     keywords = (keywords,)
 
   if ignore_first:

stem/descriptor/reader.py
@@ -263,7 +263,7 @@ class DescriptorReader(object):
   """
 
   def __init__(self, target, validate = True, follow_links = False, buffer_size = 100, persistence_path = None, document_handler = stem.descriptor.DocumentHandler.ENTRIES):
-    if isinstance(target, str):
+    if isinstance(target, (bytes, unicode)):
       self._targets = [target]
     else:
       self._targets = target

stem/exit_policy.py
@@ -108,7 +108,7 @@ def get_config_policy(rules):
   :raises: **ValueError** if input isn't a valid tor exit policy
   """
 
-  if isinstance(rules, (str, unicode)):
+  if isinstance(rules, (bytes, unicode)):
     rules = rules.split(',')
 
   result = []
@@ -147,7 +147,7 @@ class ExitPolicy(object):
   def __init__(self, *rules):
     # sanity check the types
     for rule in rules:
-      if not isinstance(rule, (str, unicode, ExitPolicyRule)):
+      if not isinstance(rule, (bytes, unicode, ExitPolicyRule)):
         raise TypeError("Exit policy rules can only contain strings or ExitPolicyRules, got a %s (%s)" % (type(rule), rules))
 
     self._rules = None          # lazily loaded series of ExitPolicyRule
@@ -304,7 +304,7 @@ class ExitPolicy(object):
       is_all_accept, is_all_reject = True, True
 
       for rule in self._input_rules:
-        if isinstance(rule, (str, unicode)):
+        if isinstance(rule, (bytes, unicode)):
           rule = ExitPolicyRule(rule.strip())
 
         if rule.is_accept:

stem/response/events.py
@@ -142,7 +142,7 @@ class Event(stem.response.ControlMessage):
     attr_values = getattr(self, attr)
 
     if attr_values:
-      if isinstance(attr_values, str):
+      if isinstance(attr_values, (bytes, unicode)):
         attr_values = [attr_values]
 
       for value in attr_values:

stem/util/connection.py
@@ -37,7 +37,7 @@ def is_valid_ipv4_address(address):
   :returns: **True** if input is a valid IPv4 address, **False** otherwise
   """
 
-  if not isinstance(address, (str, unicode)):
+  if not isinstance(address, (bytes, unicode)):
     return False
 
   # checks if theres four period separated values
@@ -105,7 +105,7 @@ def is_valid_port(entry, allow_zero = False):
         return False
 
     return True
-  elif isinstance(entry, (str, unicode)):
+  elif isinstance(entry, (bytes, unicode)):
     if not entry.isdigit():
       return False
     elif entry[0] == "0" and len(entry) > 1:

stem/util/enum.py
@@ -74,7 +74,7 @@ class Enum(object):
     keys, values = [], []
 
     for entry in args:
-      if isinstance(entry, str):
+      if isinstance(entry, (bytes, unicode)):
         key, val = entry, stem.util.str_tools._to_camel_case(entry)
       elif isinstance(entry, tuple) and len(entry) == 2:
         key, val = entry