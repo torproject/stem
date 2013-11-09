b1fb3e7a2cbbef029f483f084da98940844a3058
Damian Johnson <atagar@torproject.org>
2013-11-09 11:44:43 -0800

    Stem release 1.1.1

--- stem/__init__.py
+++ stem/__init__.py
@@ -368,7 +368,7 @@ Library for working with the tor process.
   =============== ===========
 """
 
-__version__ = '1.1.0'
+__version__ = '1.1.1'
 __author__ = 'Damian Johnson'
 __contact__ = 'atagar@torproject.org'
 __url__ = 'https://stem.torproject.org/'
