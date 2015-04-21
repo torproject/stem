2f44df7aa01ffd2aa2d68e3e07966e6785e4f1d1
Damian Johnson <atagar@torproject.org>
2015-04-20 19:54:59 -0700

    Only run installation test once
    This test is unimpacted by targets and can take a sec, so no reason to run it
    multiple times.

--- test/integ/installation.py
+++ test/integ/installation.py
@@ -10,11 +10,12 @@ import test.runner
 
 
 class TestInstallation(unittest.TestCase):
+  @test.runner.only_run_once
   def test_installing_stem(self):
     base_directory = os.path.sep.join(__file__.split(os.path.sep)[:-3])
 
     if not os.path.exists(os.path.sep.join([base_directory, 'setup.py'])):
-      test.runner.skip(self, "(only for git checkout)")
+      test.runner.skip(self, '(only for git checkout)')
 
     original_cwd = os.getcwd()
 
@@ -24,7 +25,7 @@ class TestInstallation(unittest.TestCase):
       site_packages_paths = glob.glob('/tmp/stem_test/lib/*/site-packages')
 
       if len(site_packages_paths) != 1:
-        self.fail("We should only have a single site-packages directory, but instead had: %s" % site_packages_paths)
+        self.fail('We should only have a single site-packages directory, but instead had: %s' % site_packages_paths)
 
       self.assertEqual(stem.__version__, stem.util.system.call(['python', '-c', "import sys;sys.path.insert(0, '%s');import stem;print stem.__version__" % site_packages_paths[0]])[0])
     finally:
