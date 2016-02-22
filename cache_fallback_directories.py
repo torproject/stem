9e3f6a2930893bcf4cd69a7e18c00efb2480ff10
Damian Johnson <atagar@torproject.org>
2016-02-21 18:19:16 -0800

    Script for caching fallback directories
    Oops! Forgot to add a couple new files to support fallback directories.

---
+++ cache_fallback_directories.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# Copyright 2016, Damian Johnson and The Tor Project
+# See LICENSE for licensing information
+
+"""
+Caches tor's latest fallback directories.
+"""
+
+import re
+import sys
+
+import stem.descriptor.remote
+import stem.util.conf
+import stem.util.system
+
+try:
+  # account for urllib's change between python 2.x and 3.x
+  import urllib.request as urllib
+except ImportError:
+  import urllib2 as urllib
+
+GITWEB_MAN_LOG = 'https://gitweb.torproject.org/tor.git/log/src/or/fallback_dirs.inc'
+FALLBACK_DIR_LINK = "href='/tor.git/commit/src/or/fallback_dirs.inc\?id=([^']*)'"
+
+if __name__ == '__main__':
+  try:
+    fallback_dir_page = urllib.urlopen(GITWEB_MAN_LOG).read()
+    fallback_dir_commit = re.search(FALLBACK_DIR_LINK, fallback_dir_page).group(1)
+  except:
+    print("Unable to determine the latest commit to edit tor's fallback directories: %s" % sys.exc_info()[1])
+    sys.exit(1)
+
+  try:
+    stem_commit = stem.util.system.call('git rev-parse HEAD')[0]
+  except IOError as exc:
+    print("Unable to determine stem's current commit: %s" % exc)
+    sys.exit(1)
+
+  print('Latest tor commit editing fallback directories: %s' % fallback_dir_commit)
+  print('Current stem commit: %s' % stem_commit)
+  print('')
+
+  cached_fallback_directories = stem.descriptor.remote.FallbackDirectory.from_cache()
+  latest_fallback_directories = stem.descriptor.remote.FallbackDirectory.from_remote()
+
+  if cached_fallback_directories == latest_fallback_directories:
+    print('Fallback directories are already up to date, nothing to do.')
+    sys.exit(0)
+
+  print('Differences detected...\n')
+  print(stem.descriptor.remote._fallback_directory_differences(cached_fallback_directories, latest_fallback_directories))
+
+  conf = stem.util.conf.Config()
+  conf.set('tor_commit', fallback_dir_commit)
+  conf.set('stem_commit', stem_commit)
+
+  for directory in latest_fallback_directories.values():
+    nickname = directory.nickname
+    conf.set('%s.address' % nickname, directory.address)
+    conf.set('%s.or_port' % nickname, str(directory.or_port))
+    conf.set('%s.dir_port' % nickname, str(directory.dir_port))
+    conf.set('%s.fingerprint' % nickname, directory.fingerprint)
+
+  conf.save(stem.descriptor.remote.CACHE_PATH)
