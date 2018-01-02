c9ea8a70622e80cf1ba9a960976244aa1a52ef73
Damian Johnson <atagar@torproject.org>
2018-01-01 16:40:30 -0800

    PackType enum
    Enum for a more readable counterpart for endosome's PACK_FMT.

--- stem/client.py
+++ stem/client.py
@@ -9,12 +9,36 @@ providing higher level functions in much the same way as our
 
 .. versionadded:: 1.7.0
 
+.. data:: PackType (enum)
+
+  Unsigned `struct.pack format
+  <https://docs.python.org/2/library/struct.html#format-characters>` for
+  network-order fields.
+
+  ====================  ===========
+  PackType              Description
+  ====================  ===========
+  CHAR                  Unsigned char (1 byte)
+  SHORT                 Unsigned short (2 bytes)
+  LONG                  Unsigned long (4 bytes)
+  LONG_LONG             Unsigned long long (8 bytes)
+  ====================  ===========
+
 **Module Overview:**
 
 ::
 
   Relay - Connection with a relay's ORPort.
 """
+
+from stem.util import enum
+
+PackType = enum.Enum(
+  ('CHAR', '!B'),       # 1 byte
+  ('SHORT', '!H'),      # 2 bytes
+  ('LONG', '!L'),       # 4 bytes
+  ('LONG_LONG', '!Q'),  # 8 bytes
+)
 
 
 class Relay(object):
