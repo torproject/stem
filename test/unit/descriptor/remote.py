torproject/stem

commit a4ecadb65c467fe277f205b47a2e3c93897d9891

Add stem.descriptor.get_bandwidth_file()
Tor now provides bandwidth files over its DirPort. Adding our corresponding
function to download them...

  https://trac.torproject.org/projects/tor/ticket/26902

From what I can tell these statistics are available in practice for our next
hour's consensus, but not the present one...

  atagar@morrigan:~$ curl -s 128.31.0.34:9131/tor/status-vote/next/bandwidth | wc -l
  8987

  atagar@morrigan:~$ curl -s 128.31.0.34:9131/tor/status-vote/current/bandwidth | wc -l
  0

stem/descriptor/remote.py
@@ -61,6 +61,8 @@ content. For example...
     |- get_extrainfo_descriptors - provides present extrainfo descriptors
     |- get_microdescriptors - provides present microdescriptors with the given digests
     |- get_consensus - provides the present consensus or router status entries
+    |- get_bandwidth_file - provies bandwidth heuristics used to make the next consensus
+    |- get_detached_signatures - authority signatures used to make the next consensus
     |- get_key_certificates - provides present authority key certificates
     +- query - request an arbitrary descriptor resource
 
@@ -224,6 +226,18 @@ def get_consensus(authority_v3ident = None, microdescriptor = False, **query_arg
   return get_instance().get_consensus(authority_v3ident, microdescriptor, **query_args)
 
 
+def get_bandwidth_file(**query_args):
+  """
+  Shorthand for
+  :func:`~stem.descriptor.remote.DescriptorDownloader.get_bandwidth_file`
+  on our singleton instance.
+
+  .. versionadded:: 1.8.0
+  """
+
+  return get_instance().get_bandwidth_file(**query_args)
+
+
 def get_detached_signatures(**query_args):
   """
   Shorthand for
@@ -291,6 +305,7 @@ class Query(object):
   /tor/micro/d/<hash1>-<hash2>                    microdescriptors with the given hashes
   /tor/status-vote/current/consensus              present consensus
   /tor/status-vote/current/consensus-microdesc    present microdescriptor consensus
+  /tor/status-vote/next/bandwidth                 bandwidth authority heuristics for the next consenus
   /tor/status-vote/next/consensus-signatures      detached signature, used for making the next consenus
   /tor/keys/all                                   key certificates for the authorities
   /tor/keys/fp/<v3ident1>+<v3ident2>              key certificates for specific authorities
@@ -849,6 +864,22 @@ class DescriptorDownloader(object):
 
     return self.query(resource, **query_args)
 
+  def get_bandwidth_file(self, **query_args):
+    """
+    Provides the bandwidth authority heuristics used to make the next
+    consensus.
+
+    .. versionadded:: 1.8.0
+
+    :param query_args: additional arguments for the
+      :class:`~stem.descriptor.remote.Query` constructor
+
+    :returns: :class:`~stem.descriptor.remote.Query` for the bandwidth
+      authority heuristics
+    """
+
+    return self.query('/tor/status-vote/next/bandwidth', **query_args)
+
   def get_detached_signatures(self, **query_args):
     """
     Provides the detached signatures that will be used to make the next
@@ -1070,16 +1101,22 @@ def _guess_descriptor_type(resource):
     return 'extra-info 1.0'
   elif resource.startswith('/tor/micro/'):
     return 'microdescriptor 1.0'
-  elif resource.startswith('/tor/status-vote/next/consensus-signatures'):
-    return '%s 1.0' % DETACHED_SIGNATURE_TYPE
-  elif resource.startswith('/tor/status-vote/current/consensus-microdesc'):
-    return 'network-status-microdesc-consensus-3 1.0'
-  elif resource.startswith('/tor/status-vote/'):
-    return 'network-status-consensus-3 1.0'
   elif resource.startswith('/tor/keys/'):
     return 'dir-key-certificate-3 1.0'
-  else:
-    raise ValueError("Unable to determine the descriptor type for '%s'" % resource)
+  elif resource.startswith('/tor/status-vote/'):
+    # The following resource urls can be for the present consensus
+    # (/tor/status-vote/current/*) or the next (/tor/status-vote/next/*).
+
+    if resource.endswith('/consensus'):
+      return 'network-status-consensus-3 1.0'
+    elif resource.endswith('/consensus-microdesc'):
+      return 'network-status-microdesc-consensus-3 1.0'
+    elif resource.endswith('/consensus-signatures'):
+      return '%s 1.0' % DETACHED_SIGNATURE_TYPE
+    elif resource.endswith('/bandwidth'):
+      return 'bandwidth-file 1.0'
+
+  raise ValueError("Unable to determine the descriptor type for '%s'" % resource)
 
 
 def get_authorities():

docs/_static/example/bandwidth_stats.py
@@ -1,0 +1,11 @@
+import stem.descriptor.remote
+
+bandwidth_file = stem.descriptor.remote.get_bandwidth_file().run()[0]
+
+for fingerprint, measurement in bandwidth_file.measurements.items():
+  print('Relay %s' % fingerprint)
+
+  for attr, value in measurement.items():
+    print('  %s = %s' % (attr, value))
+
+  print('')

commit 0d0f6e306a518a3989415b80542af3572bd2486d

Fix stem.descriptor.remote vote fetching
Commit a4ecadb broke our ability to download consensus votes...

  https://trac.torproject.org/projects/tor/ticket/30930

  ======================================================================
  ERROR: test_each_getter
  ----------------------------------------------------------------------
  Traceback (most recent call last):
    File "/usr/local/lib/python2.7/dist-packages/mock/mock.py", line 1305, in patched
      return func(*args, **keywargs)
    File "/home/atagar/Desktop/stem/test/unit/descriptor/remote.py", line 286, in test_each_getter
      downloader.get_vote(stem.directory.Authority.from_cache()['moria1'])
    File "/home/atagar/Desktop/stem/stem/descriptor/remote.py", line 871, in get_vote
      return self.query(resource, **query_args)
    File "/home/atagar/Desktop/stem/stem/descriptor/remote.py", line 999, in query
      return Query(resource, **args)
    File "/home/atagar/Desktop/stem/stem/descriptor/remote.py", line 460, in __init__
      self.descriptor_type = _guess_descriptor_type(resource)
    File "/home/atagar/Desktop/stem/stem/descriptor/remote.py", line 1143, in _guess_descriptor_type
      raise ValueError("Unable to determine the descriptor type for '%s'" % resource)
  ValueError: Unable to determine the descriptor type for '/tor/status-vote/current/authority'

stem/descriptor/remote.py
@@ -61,9 +61,10 @@ content. For example...
     |- get_extrainfo_descriptors - provides present extrainfo descriptors
     |- get_microdescriptors - provides present microdescriptors with the given digests
     |- get_consensus - provides the present consensus or router status entries
+    |- get_vote - provides an authority's vote for the next consensus
+    |- get_key_certificates - provides present authority key certificates
     |- get_bandwidth_file - provies bandwidth heuristics used to make the next consensus
     |- get_detached_signatures - authority signatures used to make the next consensus
-    |- get_key_certificates - provides present authority key certificates
     +- query - request an arbitrary descriptor resource
 
 .. versionadded:: 1.1.0
@@ -1119,7 +1120,7 @@ def _guess_descriptor_type(resource):
     # The following resource urls can be for the present consensus
     # (/tor/status-vote/current/*) or the next (/tor/status-vote/next/*).
 
-    if resource.endswith('/consensus'):
+    if resource.endswith('/consensus') or resource.endswith('/authority'):
       return 'network-status-consensus-3 1.0'
     elif resource.endswith('/consensus-microdesc'):
       return 'network-status-microdesc-consensus-3 1.0'

test/unit/descriptor/remote.py
@@ -270,6 +270,23 @@ class TestDescriptorDownloader(unittest.TestCase):
     self.assertEqual(1, len(descriptors))
     self.assertEqual('moria1', descriptors[0].nickname)
 
+  @patch(URL_OPEN)
+  def test_each_getter(self, dirport_mock):
+    """
+    Surface level exercising of each getter method for downloading descriptors.
+    """
+
+    downloader = stem.descriptor.remote.get_instance()
+
+    downloader.get_server_descriptors()
+    downloader.get_extrainfo_descriptors()
+    downloader.get_microdescriptors('test-hash')
+    downloader.get_consensus()
+    downloader.get_vote(stem.directory.Authority.from_cache()['moria1'])
+    downloader.get_key_certificates()
+    downloader.get_bandwidth_file()
+    downloader.get_detached_signatures()
+
   @patch(URL_OPEN, _dirport_mock(TEST_DESCRIPTOR))
   def test_reply_headers(self):
     query = stem.descriptor.remote.get_server_descriptors('9695DFC35FFEB861329B9F1AB04C46397020CE31', start = False)