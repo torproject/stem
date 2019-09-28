eba89833af2d860159b9b85d0ec52e9f2a8bcccd
Damian Johnson <atagar@torproject.org>
2019-09-28 12:58:22 -0700

    Replace riseup descriptor in unit test
    We only need a single test of a real v3 hidden service descriptor. Previously
    we used riseup for this, but George added another descriptor for what I assume
    is a testing hidden service he could decrypt. Since unit testing this new
    service provides precisely the same coverage as our riseup test combining them.

--- test/unit/descriptor/hidden_service_v3.py
+++ test/unit/descriptor/hidden_service_v3.py
@@ -25,53 +25,33 @@ expect_invalid_attr_for_text = functools.partial(base_expect_invalid_attr_for_te
 
 EXPECTED_SIGNING_CERT = """\
 -----BEGIN ED25519 CERT-----
-AQgABqKwAQVql1QZETyEwJjg+Cv6f2w/cp+c3juj01NPBaJqihboAQAgBACx+FKK
-oDrFE1+ztSxzN8sApKOb5UuDtoe/E03DxZU5+r/K5AV6G0hYn21V7Xbu2pZHvIkT
-2oVY4hypWNJE58eFBRFRzBA0J2h0GyFs1pIuRh5QDJuxB5j92V0aRCNZFgM=
+AQgABl5/AZLmgPpXVS59SEydKj7bRvvAduVOqQt3u4Tj5tVlfVKhAQAgBABUhpfe
+/Wd3p/M74DphsGcIMee/npQ9BTzkzCyTyVmDbykek2EciWaOTCVZJVyiKPErngfW
+BDwQZ8rhp05oCqhhY3oFHqG9KS7HGzv9g2v1/PrVJMbkfpwu1YK4b3zIZAk=
 -----END ED25519 CERT-----\
 """
 
 
 class TestHiddenServiceDescriptorV3(unittest.TestCase):
-  def test_for_decrypt(self):
+  def test_real_descriptor(self):
     """
-    Parse a test descriptor (used while making the v3 decode function)...
+    Parse a descriptor for a testing hidden service from asn...
 
       sltib6sxkuxh2scmtuvd5w2g7pahnzkovefxpo4e4ptnkzl5kkq5h2ad.onion
     """
 
-    if not stem.prereq.is_crypto_available(ed25519 = True):
-      self.skipTest('(requires cryptography v2.6)')
-      return
-    elif not stem.descriptor.hsv3_crypto.SHA3_AVAILABLE:
-      self.skipTest('(requires python 3.6 or pysha3)')
-      return
-
-    with open(get_resource('hidden_service_v3_test'), 'rb') as descriptor_file:
+    with open(get_resource('hidden_service_v3'), 'rb') as descriptor_file:
       desc = next(stem.descriptor.parse_file(descriptor_file, 'hidden-service-descriptor-3 1.0', validate = True))
 
     self.assertEqual(3, desc.version)
     self.assertEqual(180, desc.lifetime)
-    self.assertEqual(42, desc.revision_counter)
-
-    desc._decrypt('sltib6sxkuxh2scmtuvd5w2g7pahnzkovefxpo4e4ptnkzl5kkq5h2ad.onion')
-
-  def test_for_riseup(self):
-    """
-    Parse riseup's descriptor...
-
-      vww6ybal4bd7szmgncyruucpgfkqahzddi37ktceo3ah7ngmcopnpyyd.onion
-    """
-
-    with open(get_resource('hidden_service_v3'), 'rb') as descriptor_file:
-      desc = next(stem.descriptor.parse_file(descriptor_file, 'hidden-service-descriptor-3 1.0', validate = True, skip_crypto_validation = True))
-
-    self.assertEqual(3, desc.version)
-    self.assertEqual(180, desc.lifetime)
     self.assertEqual(EXPECTED_SIGNING_CERT, desc.signing_cert)
-    self.assertEqual(15, desc.revision_counter)
-    self.assertTrue('k9uKnDpxhkH0h1h' in desc.superencrypted)
-    self.assertEqual('wdc7ffr+dPZJ/mIQ1l4WYqNABcmsm6SHW/NL3M3wG7bjjqOJWoPR5TimUXxH52n5Zk0Gc7hl/hz3YYmAx5MvAg', desc.signature)
+    self.assertEqual(42, desc.revision_counter)
+    self.assertTrue('eaH8VdaTKS' in desc.superencrypted)
+    self.assertEqual('aglChCQF+lbzKgyxJJTpYGVShV/GMDRJ4+cRGCp+a2y/yX/tLSh7hzqI7rVZrUoGj74Xr1CLMYO3fXYCS+DPDQ', desc.signature)
+
+    if stem.prereq.is_crypto_available(ed25519 = True) and stem.descriptor.hsv3_crypto.SHA3_AVAILABLE:
+      desc._decrypt('sltib6sxkuxh2scmtuvd5w2g7pahnzkovefxpo4e4ptnkzl5kkq5h2ad.onion')
 
   def test_required_fields(self):
     """
