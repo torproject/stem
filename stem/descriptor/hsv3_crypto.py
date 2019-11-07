4db893a68a324c3ac16ece85c4f3b36e2d768fa8
Damian Johnson <atagar@torproject.org>
2019-11-07 15:22:13 -0800

    Merge ed25519_exts_ref into hsv3_crypto
    Moving the subset of ed25519_exts_ref that's used into hsv3_crypto to give
    myself one fewer modules to puzzle out.

--- stem/descriptor/hsv3_crypto.py
+++ stem/descriptor/hsv3_crypto.py
@@ -2,8 +2,7 @@ import hashlib
 import struct
 import os
 
-import stem.descriptor.ed25519_exts_ref
-import stem.descriptor.slow_ed25519
+from stem.descriptor import slow_ed25519
 
 
 """
@@ -21,6 +20,39 @@ certificate module.
 """
 
 
+def blindESK(esk, param):
+  mult = 2 ** (slow_ed25519.b - 2) + sum(2 ** i * slow_ed25519.bit(param, i) for i in range(3, slow_ed25519.b - 2))
+  s = slow_ed25519.decodeint(esk[:32])
+  s_prime = (s * mult) % slow_ed25519.l
+  k = esk[32:]
+  assert(len(k) == 32)
+  k_prime = slow_ed25519.H(b'Derive temporary signing key hash input' + k)[:32]
+  return slow_ed25519.encodeint(s_prime) + k_prime
+
+
+def blindPK(pk, param):
+  mult = 2 ** (slow_ed25519.b - 2) + sum(2 ** i * slow_ed25519.bit(param, i) for i in range(3, slow_ed25519.b - 2))
+  P = slow_ed25519.decodepoint(pk)
+  return slow_ed25519.encodepoint(slow_ed25519.scalarmult(P, mult))
+
+
+def expandSK(sk):
+  h = slow_ed25519.H(sk)
+  a = 2 ** (slow_ed25519.b - 2) + sum(2 ** i * slow_ed25519.bit(h, i) for i in range(3, slow_ed25519.b - 2))
+  k = b''.join([h[i:i + 1] for i in range(slow_ed25519.b // 8, slow_ed25519.b // 4)])
+  assert len(k) == 32
+  return slow_ed25519.encodeint(a) + k
+
+
+def signatureWithESK(m, h, pk):
+  a = slow_ed25519.decodeint(h[:32])
+  r = slow_ed25519.Hint(b''.join([h[i:i + 1] for i in range(slow_ed25519.b // 8, slow_ed25519.b // 4)]) + m)
+  R = slow_ed25519.scalarmult(slow_ed25519.B, r)
+  S = (r + slow_ed25519.Hint(slow_ed25519.encodepoint(R) + pk + m) * a) % slow_ed25519.l
+
+  return slow_ed25519.encodepoint(R) + slow_ed25519.encodeint(S)
+
+
 class HSv3PrivateBlindedKey(object):
   def __init__(self, hazmat_private_key, blinding_param):
     from cryptography.hazmat.primitives import serialization
@@ -28,14 +60,14 @@ class HSv3PrivateBlindedKey(object):
     secret_seed = hazmat_private_key.private_bytes(encoding = serialization.Encoding.Raw, format = serialization.PrivateFormat.Raw, encryption_algorithm = serialization.NoEncryption())
     assert(len(secret_seed) == 32)
 
-    expanded_identity_priv_key = stem.descriptor.ed25519_exts_ref.expandSK(secret_seed)
-    identity_public_key = stem.descriptor.slow_ed25519.publickey(secret_seed)
+    expanded_identity_priv_key = expandSK(secret_seed)
+    identity_public_key = slow_ed25519.publickey(secret_seed)
 
-    self.blinded_secret_key = stem.descriptor.ed25519_exts_ref.blindESK(expanded_identity_priv_key, blinding_param)
-    self.blinded_pubkey = stem.descriptor.ed25519_exts_ref.blindPK(identity_public_key, blinding_param)
+    self.blinded_secret_key = blindESK(expanded_identity_priv_key, blinding_param)
+    self.blinded_pubkey = blindPK(identity_public_key, blinding_param)
 
   def sign(self, msg):
-    return stem.descriptor.ed25519_exts_ref.signatureWithESK(msg, self.blinded_secret_key, self.blinded_pubkey)
+    return signatureWithESK(msg, self.blinded_secret_key, self.blinded_pubkey)
 
 
 """
