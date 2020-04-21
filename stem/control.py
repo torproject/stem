c3411da131f40e1ab79c3f7b6ee5a45d06b444f6
Illia Volochii <illia.volochii@gmail.com>
2020-04-21 22:58:07 +0300

    Make `Controller.list_ephemeral_hidden_services` asynchronous

--- stem/control.py
+++ stem/control.py
@@ -2792,7 +2792,7 @@ class Controller(BaseController):
     return True
 
   @with_default()
-  def list_ephemeral_hidden_services(self, default: Any = UNDEFINED, our_services: bool = True, detached: bool = False) -> Sequence[str]:
+  async def list_ephemeral_hidden_services(self, default: Any = UNDEFINED, our_services: bool = True, detached: bool = False) -> Sequence[str]:
     """
     list_ephemeral_hidden_services(default = UNDEFINED, our_services = True, detached = False)
 
@@ -2821,11 +2821,11 @@ class Controller(BaseController):
     result = []
 
     if our_services:
-      result += self.get_info('onions/current').split('\n')
+      result += (await self.get_info('onions/current')).split('\n')
 
     if detached:
       try:
-        result += self.get_info('onions/detached').split('\n')
+        result += (await self.get_info('onions/detached')).split('\n')
       except (stem.ProtocolError, stem.OperationFailed) as exc:
         if 'No onion services of the specified type.' not in str(exc):
           raise
