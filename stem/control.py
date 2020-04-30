163b3b383bc88cc373f9b97f8f458995f09bf498
Illia Volochii <illia.volochii@gmail.com>
2020-04-30 19:29:37 +0300

    Stop canceling asynchronous tasks for reader and event loops
    They finish automatically when detect that their controller is not alive.

--- stem/control.py
+++ stem/control.py
@@ -641,7 +641,6 @@ class BaseController(_BaseControllerSocketMixin):
     self._socket = control_socket
 
     self._asyncio_loop = asyncio.get_event_loop()
-    self._asyncio_loop_tasks = []
 
     self._msg_lock = _MsgLock()
 
@@ -784,9 +783,6 @@ class BaseController(_BaseControllerSocketMixin):
     """
 
     await self._socket.close()
-
-    for task in self._asyncio_loop_tasks:
-      task.cancel()
 
     # Join on any outstanding state change listeners. Closing is a state change
     # of its own, so if we have any listeners it's quite likely there's some
@@ -951,7 +947,7 @@ class BaseController(_BaseControllerSocketMixin):
     """
 
     for coroutine in (self._reader_loop(), self._event_loop()):
-      self._asyncio_loop_tasks.append(self._asyncio_loop.create_task(coroutine))
+      self._asyncio_loop.create_task(coroutine)
 
   async def _reader_loop(self) -> None:
     """
