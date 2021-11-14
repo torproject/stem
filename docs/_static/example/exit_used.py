4238fb495efbc1a74c6b47e793e5bb83ce3e641b
pragma31 <93341325+pragma31@users.noreply.github.com>
2021-11-14 08:42:40 +0100

    Update exit_used.py
    make sure it's a 3-hop circuit
    occasional streams going through one-hop tunnels have to be discarded, directory servers are not exits

--- docs/_static/example/exit_used.py
+++ docs/_static/example/exit_used.py
@@ -20,16 +20,17 @@ def main():
 def stream_event(controller, event):
   if event.status == StreamStatus.SUCCEEDED and event.circ_id:
     circ = controller.get_circuit(event.circ_id)
+    
+    if len(circ.path) == 3:
+      exit_fingerprint = circ.path[2][0]
+      exit_relay = controller.get_network_status(exit_fingerprint)
 
-    exit_fingerprint = circ.path[-1][0]
-    exit_relay = controller.get_network_status(exit_fingerprint)
-
-    print('Exit relay for our connection to %s' % (event.target))
-    print('  address: %s:%i' % (exit_relay.address, exit_relay.or_port))
-    print('  fingerprint: %s' % exit_relay.fingerprint)
-    print('  nickname: %s' % exit_relay.nickname)
-    print('  locale: %s' % controller.get_info('ip-to-country/%s' % exit_relay.address, 'unknown'))
-    print('')
+      print('Exit relay for our connection to %s' % (event.target))
+      print('  address: %s:%i' % (exit_relay.address, exit_relay.or_port))
+      print('  fingerprint: %s' % exit_relay.fingerprint)
+      print('  nickname: %s' % exit_relay.nickname)
+      print('  locale: %s' % controller.get_info('ip-to-country/%s' % exit_relay.address, 'unknown'))
+      print('')
 
 
 if __name__ == '__main__':
